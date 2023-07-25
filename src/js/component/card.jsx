fd4ce292e6d2781a5262dcb8a4e6806cdd484b4c
Yolanda CM <102764721+Yaoska18Carrillo@users.noreply.github.com>
2023-07-25 01:37:02 +0000

    landingpage with react terminada

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -6,7 +6,7 @@ const Card = ()  => {
         <div className="col-card-galeria">
           <div className="card-galeria">
             <img src="https://d2yoo3qu6vrk5d.cloudfront.net/images-resized/c4e94a2e-c753-4591-9501-f9dfbd4a3559-1675742367-square-original.webp" className="card-img-top" alt="..." />
-            <div className="card-body">
+            <div className="card-body text-center">
               <h5 className="card-title">Card title</h5>
               <p className="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate facere alias quaerat adipisci illum consequatur fugiat qui impedit nisi, hic corrupti quae perferendis nostrum unde culpa! Odio ea ducimus mollitia.</p>
               <a href="#" class="btn btn-primary">Find Out More!</a>
@@ -16,7 +16,7 @@ const Card = ()  => {
         <div className="col-card-galeria">
           <div className="card-galeria">
             <img src="https://d2yoo3qu6vrk5d.cloudfront.net/images-resized/c4e94a2e-c753-4591-9501-f9dfbd4a3559-1675742367-square-original.webp" className="card-img-top" alt="..." />
-            <div className="card-body">
+            <div className="card-body text-center">
               <h5 className="card-title">Card title</h5>
               <p className="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate facere alias quaerat adipisci illum consequatur fugiat qui impedit nisi, hic corrupti quae perferendis nostrum unde culpa! Odio ea ducimus mollitia.</p>
               <a href="#" class="btn btn-primary">Find Out More!</a>
@@ -26,7 +26,7 @@ const Card = ()  => {
         <div className="col-card-galeria">
           <div className="card-galeria">
             <img src="https://d2yoo3qu6vrk5d.cloudfront.net/images-resized/c4e94a2e-c753-4591-9501-f9dfbd4a3559-1675742367-square-original.webp" className="card-img-top" alt="..." />
-            <div className="card-body">
+            <div className="card-body text-center">
               <h5 className="card-title">Card title</h5>
               <p className="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate facere alias quaerat adipisci illum consequatur fugiat qui impedit nisi, hic corrupti quae perferendis nostrum unde culpa! Odio ea ducimus mollitia.</p>
               <a href="#" class="btn btn-primary">Find Out More!</a>
@@ -36,7 +36,7 @@ const Card = ()  => {
         <div className="col-card-galeria">
           <div className="card-galeria">
             <img src="https://d2yoo3qu6vrk5d.cloudfront.net/images-resized/c4e94a2e-c753-4591-9501-f9dfbd4a3559-1675742367-square-original.webp" className="card-img-top" alt="..." />
-            <div className="card-body">
+            <div className="card-body text-center">
               <h5 className="card-title">Card title</h5>
               <p className="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate facere alias quaerat adipisci illum consequatur fugiat qui impedit nisi, hic corrupti quae perferendis nostrum unde culpa! Odio ea ducimus mollitia.</p>
               <a href="#" class="btn btn-primary">Find Out More!</a>
